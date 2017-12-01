9111e96ef9b80829a416ba9e08801fe11bf92c1f
coussej <jeroen.coussement@gmail.com>
2017-12-01 16:08:12 +0100

    day 01, here we go again

---
+++ 2017/day01/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+func main() {
+	list := getList()
+	listDouble := append(list, list...)
+
+	var sum1, sum2 int
+	for i := 0; i < len(list); i++ {
+		if listDouble[i+1] == listDouble[i] {
+			sum1 += listDouble[i]
+		}
+		if listDouble[i+len(list)/2] == listDouble[i] {
+			sum2 += listDouble[i]
+		}
+	}
+
+	fmt.Printf("The solution to the first captcha is %v\n", sum1)
+	fmt.Printf("The solution to the second captcha is %v\n", sum2)
+}
+
+func getList() (list []int) {
+	in, _ := ioutil.ReadFile("input.txt")
+	for _, c := range string(in) {
+		num, _ := strconv.Atoi(string(c))
+		list = append(list, num)
+	}
+	return
+}
